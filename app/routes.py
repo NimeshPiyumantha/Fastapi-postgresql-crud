42347ec5569829d8411e946e2d034b32f848122a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-11-21 19:39:43 +0530

    Update Content

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
 from fastapi import Depends
 from config import SessionLocal
 from sqlalchemy.orm import Session
@@ -17,33 +17,43 @@ def get_db():
         db.close()
 
 
+def create_response(status: str, code: str, message: str, result=None):
+    return {
+        "status": status,
+        "code": code,
+        "message": message,
+        "result": result
+    }
+
 @router.post("/create")
 async def create_book_service(request: RequestBook, db: Session = Depends(get_db)):
-    created_book = crud.create_book(db, book=request.parameter)
-    return Response(status="Ok",
-                code="200",
-                message="Book created successfully",
-                result=created_book).dict(exclude_none=True)
-
+    try:
+        created_book = crud.create_book(db, book=request.parameter)
+        return create_response("Ok", "200", "Book created successfully", created_book)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 @router.get("/")
 async def get_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    books = crud.get_book(db, skip, limit)
-    return Response(status="Ok", code="200", message="Success fetch all data", result=books).dict(exclude_none=True)
+    try:
+        books = crud.get_book(db, skip, limit)
+        return create_response("Ok", "200", "Success fetch all data", books)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.put("/update")
 async def update_book(request: RequestBook, db: Session = Depends(get_db)):
-    up_book = crud.update_book(db, book_id=request.parameter.id,
-                             title=request.parameter.title, description=request.parameter.description)
-    return Response(status="Ok",
-                code="200",
-                message="Book update successfully",
-                result=up_book).dict(exclude_none=True)
+    try:
+        up_book = crud.update_book(db, book_id=request.parameter.id,
+                                    title=request.parameter.title, description=request.parameter.description)
+        return create_response("Ok", "200", "Book update successfully", up_book)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 @router.delete("/{id}")
 async def delete_book(id: int, db: Session = Depends(get_db)):
-    del_book = crud.remove_book(db, book_id=id)
-    return Response(status="Ok",
-                    code="200",
-                    message="Book deleted successfully",
-                    result=del_book).dict(exclude_none=True)
+    try:
+        del_book = crud.remove_book(db, book_id=id)
+        return create_response("Ok", "200", "Book deleted successfully", del_book)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
